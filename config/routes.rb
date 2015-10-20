fab211cea084374d8bbdadaf0cf0a5880ad06b23
izhilenkov <izhilenkov@gmail.com>
2015-10-21 03:08:40 +0400

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'dashboard#index'
   resources :reports
   resources :dashboard
 end
