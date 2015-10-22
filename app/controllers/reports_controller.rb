771b85edf75a1486fa7648e41fc0ef42eb6a2355
izhilenkov <zhilenkov@mercurydevelopment.com>
2015-10-22 13:09:53 +0400

    edit title

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,16 @@
 class ReportsController < ApplicationController
   def index
     if params[:branch]
-      @title = "By Branches"
+      @title = "By Branch #{params[:branch]}"
       @reports = Report.where(branch: params[:branch]).order("date DESC").all
     elsif params[:suite]
-      @title = "By Suites"
+      @title = "By Suite #{params[:suite]}"
       @reports = Report.where(suite: params[:suite]).order("date DESC").all
     elsif params[:platform]
-      @title = "By Platforms"
+      @title = "By Platform #{params[:platform]}"
       @reports = Report.where(platform: params[:platform]).order("date DESC").all
     elsif params[:server]
-      @title = "By Platforms"
+      @title = "By Server #{params[:server]}"
       @reports = Report.where(server: params[:server]).order("date DESC").all
     end
   end
