dabb54c8975fec1ed2d78183ba31fffed81bb241
izhilenkov <zhilenkov@mercurydevelopment.com>
2015-12-02 15:28:16 +0400

    add new tests

--- app/controllers/testrunner_controller.rb
+++ app/controllers/testrunner_controller.rb
@@ -10,7 +10,7 @@ class TestrunnerController < ApplicationController
 			when "BigSmoke"
 				tests = ["update_sidebar","update_schedule","schedule_document", "now", "now_dt_valid", "rate_dt_valid", "schedule_ask_dt_valid", "ask_like", "rate", "schedule_ask", "filler", "coffebreak", "update_speaker", "update_attendees", "search_attendees", "news", "update_gallery", "update_indoor", "docs"]
 			when "Networking"
-				tests = ["hands", "ask", "profile", "exitPoints"]
+				tests = ["hands", "ask", "profile", "exitPoints", "logout", "meeting", "meeting_speaker"]
 			when "Meetings"
 				tests = ["directmeeting", "canceledmeeting", "cancelmeeting", "cancelmeetinginvited"]
 		end
