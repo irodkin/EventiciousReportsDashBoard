5aa05b4b0695720824e7738bcefba012046369e6
izhilenkov <zhilenkov@mercurydevelopment.com>
2015-12-02 20:07:07 +0400

    new test choose

--- app/controllers/testrunner_controller.rb
+++ app/controllers/testrunner_controller.rb
@@ -1,18 +1,18 @@
 class TestrunnerController < ApplicationController
 	def index
-		@tests = ["networking_smoke", "navigation", "speaker", "session", "attendees", "map", "indoor", "webview", "gallery"]
+		@tests = ["smoke", "networking_smoke", "navigation", "speaker", "session", "attendees", "map", "indoor", "webview", "gallery"]
 		@jobs = Job.all
 	end
 	def tests
 		case params[:suite]
 			when "Smoke"
-				tests = ["networking_smoke", "navigation", "speaker", "session", "attendees", "map", "indoor", "webview", "gallery"]
+				tests = ["smoke", "networking_smoke", "navigation", "speaker", "session", "attendees", "map", "indoor", "webview", "gallery"]
 			when "BigSmoke"
-				tests = ["update_sidebar","update_schedule","schedule_document", "now", "now_dt_valid", "rate_dt_valid", "schedule_ask_dt_valid", "ask_like", "rate", "schedule_ask", "filler", "coffebreak", "update_speaker", "update_attendees", "search_attendees", "news", "update_gallery", "update_indoor", "docs"]
+				tests = ["bigsmoke", "update_sidebar","update_schedule", "schedule_document", "now", "now_dt_valid", "rate_dt_valid", "schedule_ask_dt_valid", "ask_like", "rate", "schedule_ask", "filler", "coffebreak", "update_speaker", "update_attendees", "search_attendees", "news", "update_gallery", "update_indoor", "docs"]
 			when "Networking"
-				tests = ["hands", "ask", "profile", "exitPoints", "logout", "meeting", "meeting_speaker"]
+				tests = ["networking", "hands", "ask", "profile", "exitPoints", "logout", "meeting", "meeting_speaker"]
 			when "Meetings"
-				tests = ["directmeeting", "canceledmeeting", "cancelmeeting", "cancelmeetinginvited"]
+				tests = ["meeting", "directmeeting", "canceledmeeting", "cancelmeeting", "cancelmeetinginvited"]
 		end
 
 		render partial: 'shared/tests', locals: {tests: tests}
