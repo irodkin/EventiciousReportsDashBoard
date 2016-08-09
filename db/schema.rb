519ea697ba62bbb5a1e4b64485649139aa3199f4
rodkin <rodkin@mercurydevelopment.com>
2016-08-09 13:44:46 +0400

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804133547) do
+ActiveRecord::Schema.define(version: 20160809092647) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
