2fcfca48f454cf6228f9bcff3aabf8a20b269d05
KorranForce <korranforce@gmail.com>
2016-09-14 13:06:04 +0400

    replace custom each actions by shorter collect in builds method of testrunner controller

--- app/controllers/testrunner_controller.rb
+++ app/controllers/testrunner_controller.rb
@@ -78,19 +78,13 @@ class TestrunnerController < ApplicationController
 		queue_count=0
 		if job_info["inQueue"]
 			Nokogiri::XML(RestClient.get("http://jenkins.mercury.office:8080/queue/api/xml?tree=items[actions[parameters[*]],blocked,buildable,id,inQueueSince,stuck,task[name],why,pending]&xpath=/queue/item[task/name='#{params[:job]}']&wrapper=queue")).xpath('./queue/item').each {|i|
-				params=[]
-				i.xpath('./action/parameter').each {|p|
-					params << "#{p.xpath('./name').text}=#{p.xpath('./value').text}"
-				}
+				params = i.xpath('./action/parameter').collect {|p| "#{p.xpath('./name').text}=#{p.xpath('./value').text}"}
 				builds << {"status"=>"IN_QUEUE", "params"=>params}
 				queue_count+=1
 			}
 		end
 		job_info["builds"].each {|b|
-			params=[]
-			b["actions"][0]["parameters"].each {|p|
-				params << "#{p["name"]}=#{p["value"]}"
-			}
+			params = b["actions"][0]["parameters"].collect {|p| "#{p["name"]}=#{p["value"]}"}
 			builds << {"number"=>b["number"], "status"=>b["building"] ? "IN_PROGRESS" : b["result"], "params"=>params}
 		}
 		while queue_count>0
