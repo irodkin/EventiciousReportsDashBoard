ccb2d58ee82839053d115a99946b65624f04474a
KorranForce <korranforce@gmail.com>
2016-10-12 18:41:11 +0400

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010150621) do
+ActiveRecord::Schema.define(version: 20161012142357) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
