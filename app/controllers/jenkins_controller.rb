0d4c8c6b73c11452f2f06b993147d5afd42288e5
KorranForce <korranforce@gmail.com>
2017-07-06 16:36:08 +0400

    add timeout to request to jenkins in current_builds; code style in jenkins controller

--- app/controllers/jenkins_controller.rb
+++ app/controllers/jenkins_controller.rb
@@ -1,21 +1,28 @@
 class JenkinsController < ApplicationController
 	def initialize
-		@status_images = {"ABORTED"=>"#{JENKINS_URL}static/aaf3e3e1/images/16x16/aborted.png",
-		                  "SUCCESS"=>"#{JENKINS_URL}static/aaf3e3e1/images/16x16/blue.png",
-		                  "FAILURE"=>"#{JENKINS_URL}static/aaf3e3e1/images/16x16/red.png",
-		                  "IN_PROGRESS"=>"#{JENKINS_URL}static/aaf3e3e1/images/16x16/blue_anime.gif",
-		                  "IN_QUEUE"=>"#{JENKINS_URL}static/aaf3e3e1/images/16x16/hourglass.png",
-		                  "UNKNOWN"=>"questionmark.png"}
+		@status_images = {"ABORTED"=> "#{JENKINS_URL}static/aaf3e3e1/images/16x16/aborted.png",
+		                  "SUCCESS"=> "#{JENKINS_URL}static/aaf3e3e1/images/16x16/blue.png",
+		                  "FAILURE"=> "#{JENKINS_URL}static/aaf3e3e1/images/16x16/red.png",
+		                  "IN_PROGRESS"=> "#{JENKINS_URL}static/aaf3e3e1/images/16x16/blue_anime.gif",
+		                  "IN_QUEUE"=> "#{JENKINS_URL}static/aaf3e3e1/images/16x16/hourglass.png",
+		                  "UNKNOWN"=> "questionmark.png"}
 	end
 	def builds
 		#RestClient::Exceptions::ReadTimeout
-		job_info = JSON.parse(RestClient::Request.execute(method: :get, url: "#{JENKINS_URL}job/#{params[:job]}/api/json?pretty=true&tree=builds[actions[parameters[*]],building,number,result,url,builtOn]{0,20},nextBuildNumber,inQueue", timeout:5))
+		job_info = JSON.parse(RestClient::Request.execute(method: :get,
+		                                                  url: "#{JENKINS_URL}job/#{params[:job]}/api/json?pretty=true&tree="\
+		                                                       "builds[actions[parameters[*]],building,number,result,url,builtOn]{0,20},nextBuildNumber,inQueue",
+		                                                  timeout:5))
 		builds = []
 		queue_count = 0
 		if job_info["inQueue"]
-			Nokogiri::XML(RestClient.get("#{JENKINS_URL}queue/api/xml?tree=items[actions[parameters[*]],blocked,buildable,id,inQueueSince,stuck,task[name],why,pending]&xpath=/queue/item[task/name='#{params[:job]}']&wrapper=queue")).xpath('./queue/item').each {|i|
+			Nokogiri::XML(RestClient::Request.execute(method: :get,
+			                                          url: "#{JENKINS_URL}queue/api/xml"\
+			                                               "?tree=items[actions[parameters[*]],blocked,buildable,id,inQueueSince,stuck,task[name],why,pending]"\
+			                                               "&xpath=/queue/item[task/name='#{params[:job]}']&wrapper=queue",
+			                                          timeout:5)).xpath('./queue/item').each {|i|
 				params = i.xpath('./action/parameter').collect {|p| "#{p.xpath('./name').text}=#{p.xpath('./value').text}"}
-				builds << {"status"=>"IN_QUEUE", "params"=>params}
+				builds << {"status"=> "IN_QUEUE", "params"=> params}
 				queue_count += 1
 			}
 			queue_count -= 1
@@ -28,7 +35,7 @@ class JenkinsController < ApplicationController
 		job_info["builds"].each {|b|
 			#find because order of hash with parameters key is not constant in array
 			params = b["actions"].find {|h| h.has_key?("parameters")}["parameters"].collect {|p| "#{p["name"]}=#{p["value"]}"}
-			builds << {"number"=>b["number"], "status"=>b["building"] ? "IN_PROGRESS" : b["result"], "params"=>params}
+			builds << {"number"=> b["number"], "status"=> b["building"] ? "IN_PROGRESS" : b["result"], "params"=> params}
 		}
 		render partial: 'shared/buildstable', locals: {builds: builds}
 	end
@@ -37,16 +44,24 @@ class JenkinsController < ApplicationController
 		Job.find_each {|job|
 			job_title = job.title
 			begin
-				job_info = JSON.parse(RestClient::Request.execute(method: :get, url: "#{JENKINS_URL}job/#{job_title}/api/json?pretty=true&tree=lastBuild[actions[parameters[*]],building,number,result,url,builtOn]{0,20},nextBuildNumber,inQueue", timeout:5))
+				job_info = JSON.parse(RestClient::Request.execute(method: :get,
+				                                                  url: "#{JENKINS_URL}job/#{job_title}/api/json?pretty=true&tree="\
+				                                                       "lastBuild[actions[parameters[*]],building,number,result,url,builtOn]{0,20},nextBuildNumber,inQueue",
+				                                                  timeout:5))
 			rescue => e
 				puts e
 				build = {"number"=>e.message, "status"=>"UNKNOWN", "link"=>"#{JENKINS_URL}job/#{job_title}", "link_text"=>"Job"}
 			else
 				if job_info["lastBuild"]["result"] && job_info["inQueue"]
-					#Nokogiri::XML(RestClient.get("#{JENKINS_URL}queue/api/xml?tree=items[actions[parameters[*]],blocked,buildable,id,inQueueSince,stuck,task[name],why,pending]&xpath=/queue/item[task/name='#{params[:job]}']&wrapper=queue")).xpath('./queue/item').each {|i|
-					build = {"number"=>"##{job_info["nextBuildNumber"]}", "status"=>"IN_QUEUE", "link"=>"#{JENKINS_URL}job/#{job_title}", "link_text"=>"Job"}
+					build = {"number"=> "##{job_info["nextBuildNumber"]}",
+					         "status"=> "IN_QUEUE",
+					         "link"=> "#{JENKINS_URL}job/#{job_title}",
+					         "link_text"=> "Job"}
 				else
-					build = {"number"=>"##{job_info["lastBuild"]["number"]}", "status"=>job_info["lastBuild"]["building"] ? "IN_PROGRESS" : job_info["lastBuild"]["result"], "link"=>"#{JENKINS_URL}job/#{job_title}/#{job_info["lastBuild"]["number"]}/parameters", "link_text"=>"Build parameters"}
+					build = {"number"=> "##{job_info["lastBuild"]["number"]}",
+					         "status"=> job_info["lastBuild"]["building"] ? "IN_PROGRESS" : job_info["lastBuild"]["result"],
+					         "link"=> "#{JENKINS_URL}job/#{job_title}/#{job_info["lastBuild"]["number"]}/parameters",
+					         "link_text"=> "Build parameters"}
 				end
 			end
 			current_builds << {job_title: job_title, build: build}
