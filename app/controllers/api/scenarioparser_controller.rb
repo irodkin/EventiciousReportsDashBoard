7408f2106ee44b2c45efa40a017eebb413448f92
KorranForce <korranforce@gmail.com>
2017-09-29 17:55:31 +0400

    improve parse method of ScenarioparserController

--- app/controllers/api/scenarioparser_controller.rb
+++ app/controllers/api/scenarioparser_controller.rb
@@ -30,9 +30,9 @@ class Api::ScenarioparserController < ApplicationController
 
 	def delete_feature
 		feature = Suite.find(params[:id])
-		scenarios_id = Test.where(suite: feature.title).all.collect {|st| st.id}
+		scenarios_id = Test.where(suite: feature.title).all.collect {|scenario| scenario.id}
 		if feature.destroy
-			Test.where(suite: feature.title).find_each {|s| s.destroy}
+			Test.where(suite: feature.title).find_each {|scenario| scenario.destroy}
 			render json: {feature: feature.title,
 			              scenarios: scenarios_id,
 			              deleted: true},
@@ -55,22 +55,18 @@ class Api::ScenarioparserController < ApplicationController
 	end
 
 	def parse
-		parser = Gherkin3::Parser.new
-		parsed_feature = parser.parse(params[:suite])
-		tags = parsed_feature[:tags].each {|t| t[:name]}
+		parsed_feature = Gherkin3::Parser.new.parse(params[:suite])
+		#feature_tags = parsed_feature[:tags].collect {|feature_tag| feature_tag[:name]}
 		feauture_name = parsed_feature[:name]
-		scenarios = parsed_feature[:scenarioDefinitions]
-		scenario = []
-		scenarios.collect! {|s|
-			scenario.push({suite: feauture_name,
-			               tags: s[:tags].collect {|t| t[:name].delete!('@') }.join(","),
-			               title: s[:name],
-			               steps: s[:steps].collect {|st| "<div><span class=\"keyword\">#{st[:keyword]}</span> #{st[:text]}</div>"}.join("")})
+		scenarios = parsed_feature[:scenarioDefinitions].collect {|scenario|
+			tags = scenario[:tags].collect {|tag| tag[:name].delete('@')}.join(",")
+			steps = scenario[:steps].collect {|step| "<div><span class=\"keyword\">#{step[:keyword]}</span> #{step[:text]}</div>"}.join("")
+			{suite: feauture_name,
+			 tags: tags,
+			 title: scenario[:name],
+			 steps: steps}
 		}
-		scenario.each do |s|
-			t = Test.new(s)
-			t.save
-		end
+		scenarios.each {|scenario| Test.new(scenario).save}
 		scenarios = Test.where(suite: params[:feature])
 		render partial: 'shared/scenarios',
 		       locals: {scenarios: scenarios, edit: false}
