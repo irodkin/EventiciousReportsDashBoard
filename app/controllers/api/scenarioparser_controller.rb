705ee492dcb51719b5807de38b6d90d773bb47af
KorranForce <korranforce@gmail.com>
2017-09-29 16:59:47 +0400

    code style

--- app/controllers/api/scenarioparser_controller.rb
+++ app/controllers/api/scenarioparser_controller.rb
@@ -2,86 +2,77 @@ require 'gherkin3/parser'
 
 class Api::ScenarioparserController < ApplicationController
 
-  def get_tests
-    render json: {
-      :suite => Test.where(suite: params[:suite]).all
-    },
-      status: 200
-  end
+	def get_tests
+		render json: {suite: Test.where(suite: params[:suite]).all},
+		       status: 200
+	end
 
-  def get_test
-  	tests = Test.all
-  	needed = tests.find {|t| (t[:tags].to_s.split(",")).include?(params[:tag])}
-    render json: {
-      :tests => needed
-    },
-      status: 200
-  end
+	def get_test
+		tests = Test.all
+		needed = tests.find {|t| t[:tags].to_s.split(",").include?(params[:tag])}
+		render json: {tests: needed},
+		       status: 200
+	end
 
-  def add_feature
-    s = Suite.new(:title=>params[:title], :tag=>params[:tag])
-    if s.save
-      suites = Suite.all
-      render partial: 'shared/featurelist', locals: {suites: suites}
-    else
-      render json: {
-        :suk => "error"
-      }
-    end
-  end
+	def add_feature
+		s = Suite.new(title: params[:title], tag: params[:tag])
+		if s.save
+			suites = Suite.all
+			render partial: 'shared/featurelist', locals: {suites: suites}
+		else
+			render json: {suk: "error"}
+		end
+	end
 
-  def get_features
-    Suite.all
-  end
+	def get_features
+		Suite.all
+	end
 
-  def delete_feature
-    feature = Suite.find(params[:id])
-    scenarios_id = Test.where(suite: feature.title).all.collect! { |st| st.id }
-    if feature.destroy
-      Test.where(suite: feature.title).find_each { |s| s.destroy }
-      render json: {
-                    :feature=>feature.title,
-                    :scenarios=>scenarios_id,
-                    :deleted=>true
-                  },
-             status: 200
-    else
-      render json: { :deleted=>false },
-             status: 500
-    end
-  end
+	def delete_feature
+		feature = Suite.find(params[:id])
+		scenarios_id = Test.where(suite: feature.title).all.collect {|st| st.id}
+		if feature.destroy
+			Test.where(suite: feature.title).find_each {|s| s.destroy}
+			render json: {feature: feature.title,
+			              scenarios: scenarios_id,
+			              deleted: true},
+			       status: 200
+		else
+			render json: {deleted: false},
+			       status: 500
+		end
+	end
 
-  def delete_scenario
-    scenario = Test.find(params[:id])
-    if scenario.destroy
-      render json: { :deleted=>true },
-             status: 200
-    else
-      render json: { :deleted=>false },
-             status: 500
-    end
-  end
+	def delete_scenario
+		scenario = Test.find(params[:id])
+		if scenario.destroy
+			render json: {deleted: true},
+			       status: 200
+		else
+			render json: {deleted: false},
+			       status: 500
+		end
+	end
 
-  def parse
-    parser = Gherkin3::Parser.new
-    parsed_feature = parser.parse(params[:suite])
-    tags = parsed_feature[:tags].each { |t| t[:name] }
-    feauture_name = parsed_feature[:name]
-    scenarios = parsed_feature[:scenarioDefinitions]
-    scenario = []
-    scenarios.collect!  do |s|
-    	scenario.push(
-    	 :suite => feauture_name,
-       :tags =>(s[:tags].collect! { |t| t[:name].delete!('@') }).join(","),
-    	 :title =>s[:name],
-    	 :steps => (s[:steps].collect! { |st| "<div><span class=\"keyword\">#{st[:keyword]}</span> #{st[:text]}</div>"}).join("")
-         )
-      end
-    scenario.each do |s|
-      t = Test.new(s)
-      t.save
-    end
-    scenarios = Test.where(suite: params[:feature])
-    render partial: 'shared/scenarios', locals: {scenarios: scenarios, edit: false}
-  end
+	def parse
+		parser = Gherkin3::Parser.new
+		parsed_feature = parser.parse(params[:suite])
+		tags = parsed_feature[:tags].each {|t| t[:name]}
+		feauture_name = parsed_feature[:name]
+		scenarios = parsed_feature[:scenarioDefinitions]
+		scenario = []
+		scenarios.collect! {|s|
+			scenario.push({suite: feauture_name,
+			               tags: s[:tags].collect {|t| t[:name].delete!('@') }.join(","),
+			               title: s[:name],
+			               steps: s[:steps].collect {|st| "<div><span class=\"keyword\">#{st[:keyword]}</span> #{st[:text]}</div>"}.join("")})
+		}
+		scenario.each do |s|
+			t = Test.new(s)
+			t.save
+		end
+		scenarios = Test.where(suite: params[:feature])
+		render partial: 'shared/scenarios',
+		       locals: {scenarios: scenarios, edit: false}
+	end
 end
--- app/controllers/testrunner_controller.rb
+++ app/controllers/testrunner_controller.rb
@@ -10,27 +10,29 @@ class TestrunnerController < ApplicationController
 		suite_title = params[:suite]
 		suite_tag = Suite.where(title: params[:suite]).first.tag
 		edit = params[:edit] == 'true' ? true : false
-		render partial: 'shared/scenarios', locals: {scenarios: scenarios, edit: edit, suite_title: suite_title, suite_tag: suite_tag}
+		render partial: 'shared/scenarios',
+		       locals: {scenarios: scenarios,
+		                edit: edit,
+		                suite_title: suite_title,
+		                suite_tag: suite_tag}
 	end
 	def retry_run_params
 		report = Report.find(params[:report_id])
-		render json: {
-			platform: report.platform,
-			branch: report.branch,
-			appid: report.appid,
-			app_type: report.app_type,
-			api_version: report.api_version,
-			locale: report.locale,
-			suite: report.suite,
-			server: report.server,
-			job: report.job.title
-		},
-		status: 200
+		render json: {platform: report.platform,
+		              branch: report.branch,
+		              appid: report.appid,
+		              app_type: report.app_type,
+		              api_version: report.api_version,
+		              locale: report.locale,
+		              suite: report.suite,
+		              server: report.server,
+		              job: report.job.title},
+		       status: 200
 	end
 	def retry_all
 		report = Report.find(params[:report_id])
 		if report.tests.kind_of?(Array)
-			tests = report.tests.each { |t| t.delete("@")}
+			tests = report.tests.each {|t| t.delete("@")}
 		else
 			tests = report.tests.delete("@")
 		end
@@ -39,37 +41,36 @@ class TestrunnerController < ApplicationController
 	end
 	def retry_failed
 		report = Report.find(params[:report_id])
-		render json: {
-			failed_tests: report.failed_tests,
-			pending_tests: report.pending_tests
-		},
-		status: 200
+		render json: {failed_tests: report.failed_tests,
+		              pending_tests: report.pending_tests},
+		       status: 200
 	end
 	def tests
 		tests = Test.where(suite: params[:suite]).all
-		tags_arr = tests.collect { |t| t.tags}
+		tags_arr = tests.collect {|t| t.tags}
 		split = []
 		to_delete = []
 		tags_arr.collect! do |t|
-			if t.split(",").size>1
-				t.split(",").each {|t| split.push t }
+			if t.split(",").size > 1
+				t.split(",").each {|t| split.push(t)}
 				to_delete.push t
 			end
 			t
 		end
-		to_delete.each {|d| tags_arr.delete d}
+		to_delete.each {|d| tags_arr.delete(d)}
 		split.each {|s| tags_arr.push s}
 		scenarios = []
 		tags_arr.uniq!
 		tags_arr.each do |t|
-			scenarios_a = tests.find_all { |f| f.tags.split(",").include?(t)}
+			scenarios_a = tests.find_all {|f| f.tags.split(",").include?(t)}
 			scenarios_b = []
-			scenarios_a.each do |s|
+			scenarios_a.each {|s|
 				scenarios_b.push "<div><span class=\"keyword\">Scenario: </span><span class=\"scenario_title\">#{s.title}<span></div><div style=\"margin-left: 5px\">#{s.steps}</div>"
-			end
+			}
 			scenarios.push([scenarios_b, t])
 		end
 		head_tag = Suite.where(title: params[:suite]).first.tag
-		render partial: 'shared/tests', locals: {tags: scenarios, head_tag: head_tag}
+		render partial: 'shared/tests',
+		       locals: {tags: scenarios, head_tag: head_tag}
 	end
 end
